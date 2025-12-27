6f4874c77b7a5ab30af0ab1eeda5c96fb6f61a08
thenileshmishra <thenileshmishra@gmail.com>
2025-12-27 16:43:35 +0530

    feat: add config and constants

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    APP_NAME: str = "Enterprise Policy RAG"
+    ENVIRONMENT: str = "Dev"
+    LOG_LEVEL: str = "INFO"
+
+    class Config:
+        env_file = ".env"
+        
+settings = Settings()
